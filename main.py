2ba82121477284e79b89a15fa754595003cc21e6
Mpk320 <mpkasunick@gmail.com>
2021-11-13 05:57:39 -0500

    It loops now + notes

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+# Calculates velocity of an object
 
-position = input("Input Change in position(m): ")
-time = input("input Time(s): ")
+# where calculation takes place
+def data():
+    position = input("Input Change in position(m): ")
+    time = input("input Time(s): ")
+    result = float(position) / float(time)
+    print("Velocity(m/s) = " + str(result))
 
-result = int(position) / int(time)
 
-print("Velocity(m/s) = " + str(result))
+# to reset calc
+def retry():
+    redo = input("Enter y to continue or n to quit: ")
+    if redo == "y":
+        return beginning()
+    elif redo == "n":
+        print("Have a nice day!")
+    else:
+        wrong_data()
+
+
+# loops retry function if y/n are not received
+def wrong_data():
+    retry()
+
+
+# runs data and retry functions
+def beginning():
+    data()
+    retry()
+
+
+beginning()
